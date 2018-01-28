db4624c55aebda7a95b63e97be9b01d4e73d2b4e
Toshiki Hata <t.hata@oratta.net>
2018-01-28 12:41:01 +0900

    Initial Commit

---
+++ HellowWorldObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HellowWorldObjC
+//
+//  Created by Hata on 2018/01/28.
+//  Copyright © 2018年 th1209. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
